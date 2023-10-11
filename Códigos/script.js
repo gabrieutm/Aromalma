50d7b866cc47dfbf5887a4367f66a03bd9d439c7
gabrieutm <gtmachado08@gmail.com>
2023-10-11 17:23:04 -0300

    upload

---
+++ Códigos/script.js
@@ -0,0 +1,85 @@
+let time = 5000,
+    currentImageIndex = 0,
+    images = document.querySelectorAll("#container img"),
+    max = images.length;
+
+function nextImage() {
+    images[currentImageIndex].classList.remove("selected")
+
+    currentImageIndex++
+
+    if (currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex].classList.add("selected")
+};
+
+function start() {
+    setInterval(() => {
+        nextImage()
+    }, time)
+};
+
+window.addEventListener("load", start);
+
+
+//sidebar
+const openmenu = document.getElementById("open-menu");
+const closemenu = document.getElementById("close-menu");
+const sidebar = document.getElementById("sidebar");
+
+openmenu.addEventListener("click", function() {
+    sidebar.style.left = "50px";
+    openmenu.style.opacity = "0";
+});
+
+closemenu.addEventListener("click", function() {
+    sidebar.style.left = "-300px";
+    openmenu.style.opacity = "1";
+});
+
+
+
+//menus
+function toggleMenu(menuId) {
+    const menu = document.getElementById(menuId);
+    const menus = document.querySelectorAll('.menu-popup');
+
+    menus.forEach(function (m) {
+        if (m !== menu) {
+            m.style.display = 'none'; // Fecha todos os outros menus
+        }
+    });
+
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
+
+
+
+//fecha todos os menus
+function closeAllMenus() {
+    const menus = document.querySelectorAll('.menu-popup');
+
+    menus.forEach(function (menu) {
+        menu.style.display = 'none';
+    });
+}
+
+
+//tela de login e cadastro
+function toggleScreen(screen) {
+    const loginContainer = document.getElementById("login-container");
+    const registerContainer = document.getElementById("register-container");
+
+    if (screen === 'login') {
+        loginContainer.style.display = 'block';
+        registerContainer.style.display = 'none';
+    } else if (screen === 'register') {
+        loginContainer.style.display = 'none';
+        registerContainer.style.display = 'block';
+    }
+}
